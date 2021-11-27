f0928041739beb3fe13ab944e736f897457dae69
Amit Gautam <amgautam@ciena.com>
2021-11-27 11:39:15 +0530

    todo list created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 
 function App() {
+  const [list, setList] = React.useState([])
+  const [todo, setTodo] = React.useState('')
+
+  function handleDelete(index){
+    const newList = [...list]
+    newList.splice(index, 1)
+    setList(newList)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+      <input value={todo} onChange={e=>setTodo(e.target.value)}/>
+      <button onClick={e=>setList([...list, todo])}>
+        create
+      </button>
+      </div>
+      <div className='list-container'>
+      {list.map((entry, idx) => <ListEntry key={idx} todo={entry} index={idx} delete={index => handleDelete(index)}/>)}
+      </div>
     </div>
   );
+}
+
+function ListEntry(props){
+  const {todo, index} = props
+  return(
+    <div className='list' key={index}>
+      {todo}
+      <button onClick={e=>props.delete(index)}>
+        delete
+      </button>
+    </div>
+  )
 }
 
 export default App;
